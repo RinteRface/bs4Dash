e154713ea7ecd46592059a1bd5d311217b7bfbf8
Divad0175 <dgranjon@ymail.com>
2018-06-27 09:30:14 +0200

    add bs4DashGallery and update website

---
+++ R/bs4DashGallery.R
@@ -0,0 +1,23 @@
+#' @title Launch the bs4Dash Gallery
+#'
+#' @description A gallery of all components available in bs4Dash.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' if (interactive()) {
+#'
+#'  bs4DashGallery()
+#'
+#' }
+bs4DashGallery <- function() { # nocov start
+  if (!requireNamespace(package = "fontawesome"))
+    message("Package 'fontawesome' is required to run this function")
+  if (!requireNamespace(package = "shinyWidgets"))
+    message("Package 'shinyWidgets' is required to run this function")
+  if (!requireNamespace(package = "plotly"))
+    message("Package 'plotly' is required to run this function")
+  
+  shiny::shinyAppFile(system.file('examples/showcase/app.R', package = 'bs4Dash', mustWork = TRUE))
+}
