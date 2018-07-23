6b3020cf207806c721b23f557b1695272b77d320
Divad0175 <dgranjon@ymail.com>
2018-07-23 10:06:33 +0200

    fix #7 (@theRcast), add a "fixed-top" navbar option

--- NEWS.md
+++ NEWS.md
@@ -1,6 +1,7 @@
 # bs4Dash 0.2.0.9000
 - update `bs4DashGallery()`. Add a "theme" argument
 - fix vignette titles
+- add a "fixed" argument to the `bs4DashNavbar()` (#7, thanks @theRcast)
 - update Readme
 
 # bs4Dash 0.1.0
--- R/dashboardNavbar.R
+++ R/dashboardNavbar.R
@@ -11,17 +11,21 @@
 #' @param controlbarIcon Icon to toggle the controlbar (left).
 #' @param leftUi Custom left Ui content. Any Ui element.
 #' @param rightUi Custom right Ui content. Any Ui element.
+#' @param fixed Whether the navbar is fixed to the top. FALSE by default
 #'
 #' @author David Granjon, \email{dgranjon@@ymail.com}
 #'
 #' @export
 bs4DashNavbar <- function(..., skin = "light", status = "white", border = TRUE,
                                sidebarIcon = "bars", controlbarIcon = "th",
-                               leftUi = NULL, rightUi = NULL) {
+                               leftUi = NULL, rightUi = NULL, fixed = FALSE) {
 
   navbarTag <- shiny::tags$nav(
-    class = paste0("main-header navbar navbar-expand bg-", status,
-                   " navbar-", skin, if (isTRUE(border)) " border-bottom" else NULL),
+    class = paste0(
+      "main-header navbar navbar-expand bg-", status,
+      " navbar-", skin, if (isTRUE(border)) " border-bottom" else NULL,
+      if (fixed) " fixed-top" else NULL
+    ),
 
     # left sidebar elements
     shiny::tags$ul(
