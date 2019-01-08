48b5d04c28c09edd5849f9bbcdea6a3e33307133
Divad0175 <dgranjon@ymail.com>
2019-01-08 13:23:56 +0100

    write cleaner jQuery

--- inst/bs4Dash-0.2.0/bs4Dash.js
+++ inst/bs4Dash-0.2.0/bs4Dash.js
@@ -30,14 +30,13 @@ $(function () {
   var workerId = $('base').attr('href');
   // ensure that this code does not run on shiny server/pro and locally
   if (typeof workerId != "undefined") {
-    // prevent this from being infinitely loaded
-    var counter = 0;
-    if (counter === 0) {
      // get the initial page url
     var url = window.location.href;
-    // replace the url by the url for shinyapp.io
-    window.location.replace(url + workerId); 
-    counter += 1;
+    var redirect = url + workerId;
+    // prevent from infinitely loading
+    if (url !== redirect) {
+      // replace the url by the url for shinyapp.io
+      window.location.replace(redirect);  
     }
   }
   
