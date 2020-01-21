e6240ab9d15feb70390b13eccc261a5f08c9d287
DivadNojnarg <dgranjon@ymail.com>
2020-01-22 00:12:29 +0100

    add helpers for bs4Card test

--- tests/testthat/test-cards.R
+++ tests/testthat/test-cards.R
@@ -1,5 +1,48 @@
 context("bs4Card")
 
+getCardWrapperCl <- function(card) {
+  card$attribs$class
+}
+
+getCardCl <- function(card) {
+  card$children[[1]]$attribs$class
+}
+
+getCardId <- function(card) {
+  card$children[[1]]$attribs$id
+}
+
+getCardChildren <- function(card) {
+  bs4Dash:::dropNulls(card$children[[1]]$children)
+}
+
+
+getCardHeader <- function(card) {
+  getCardChildren(card)[[1]]
+}
+
+getCardHeaderCl <- function(card) {
+  header <- getCardHeader(card)
+  header$attribs$class
+}
+
+getCardBody <- function(card) {
+  bs4Dash:::dropNulls(getCardChildren(card)[[2]]$children)
+}
+
+getCardFooter <- function(card) {
+  getCardChildren(card)[[3]]
+}
+
+getCardBodyStyle <- function(card) {
+  card$children[[1]]$children[[2]]$attribs$style
+}
+
+getCardTools <- function(card) {
+  header <- getCardHeader(card)
+  bs4Dash:::dropNulls(header$children[[2]]$children)
+}
+
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
