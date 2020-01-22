150d5f1a495b30b26fb1606121b6e8d2fb68d591
DivadNojnarg <dgranjon@ymail.com>
2020-01-22 08:15:37 +0100

    add helpers for bs4ValueBox test

--- tests/testthat/test-bs4ValueBox.R
+++ tests/testthat/test-bs4ValueBox.R
@@ -1,3 +1,13 @@
 context("bs4ValueBox")
 
+getCardWrapperCl <- function(card) {
+  card$attribs$class
+}
 
+getCardCl <- function(card) {
+  card$children[[1]]$attribs$class
+}
+
+getCardChildren <- function(card) {
+  bs4Dash:::dropNulls(card$children[[1]]$children)
+}
