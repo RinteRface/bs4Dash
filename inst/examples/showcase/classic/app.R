183eaa2c74c6b4fc80c8f74706289c703670fd59
DivadNojnarg <dgranjon@ymail.com>
2020-11-25 18:09:15 +0100

    add custom area in sidebar example

--- inst/examples/showcase/classic/app.R
+++ inst/examples/showcase/classic/app.R
@@ -26,8 +26,8 @@ shinyApp(
         title = "Toggle button",
         content = "This toggle the left sidebar",
         placement = "bottom",
-        #`data-trigger` = "hover",
-        actionButton(inputId = "sidebarToggle", label = "Toggle left sidebar", class = "mx-2") 
+        # `data-trigger` = "hover",
+        actionButton(inputId = "sidebarToggle", label = "Toggle left sidebar", class = "mx-2")
       ),
       rightUi = tagList(
         dropdownMenu(
@@ -71,6 +71,17 @@ shinyApp(
       skin = "light",
       status = "primary",
       id = "sidebar",
+      customArea = fluidRow(
+        actionButton(
+          inputId = "myAppButton",
+          label = NULL,
+          icon = icon("users"),
+          width = NULL,
+          status = "primary",
+          style = "margin: auto",
+          dashboardBadge(textOutput("btnVal"), color = "danger")
+        )
+      ),
       sidebarMenu(
         id = "current_tab",
         flat = FALSE,
@@ -113,15 +124,15 @@ shinyApp(
           tabName = "valueboxes",
           icon = icon("suitcase")
         ),
-        
+
         sidebarHeader("Colors"),
-        
+
         menuItem(
           "Colors",
           tabName = "colors",
           icon = icon("tint")
         ),
-        
+
         sidebarHeader("BS4 gallery"),
         menuItem(
           text = "Galleries",
@@ -130,10 +141,10 @@ shinyApp(
           menuSubItem(
             text = HTML(
               paste(
-                "Gallery 1", 
+                "Gallery 1",
                 dashboardBadge(
-                  "new", 
-                  position = "right", 
+                  "new",
+                  position = "right",
                   color = "danger"
                 )
               )
@@ -144,10 +155,10 @@ shinyApp(
           menuSubItem(
             text = HTML(
               paste(
-                "Gallery 2", 
+                "Gallery 2",
                 dashboardBadge(
-                  "!", 
-                  position = "right", 
+                  "!",
+                  position = "right",
                   color = "success"
                 )
               )
@@ -197,12 +208,14 @@ shinyApp(
             width = 12,
             align = "center",
             radioButtons(
-              inputId = "dist", 
+              inputId = "dist",
               label = "Distribution type:",
-              c("Normal" = "norm",
+              c(
+                "Normal" = "norm",
                 "Uniform" = "unif",
                 "Log-normal" = "lnorm",
-                "Exponential" = "exp")
+                "Exponential" = "exp"
+              )
             )
           )
         ),
@@ -215,7 +228,7 @@ shinyApp(
     footer = dashboardFooter(
       fixed = FALSE,
       left = a(
-        href = "https://twitter.com/divadnojnarg", 
+        href = "https://twitter.com/divadnojnarg",
         target = "_blank", "@DivadNojnarg"
       ),
       right = "2020"
@@ -223,8 +236,13 @@ shinyApp(
     title = "bs4Dash Showcase"
   ),
   server = function(input, output, session) {
-    
     useAutoColor()
+
+    # app button --------------------------------------------------------------
+    output$btnVal <- renderText(input$myAppButton)
+    observeEvent(input$myAppButton, {
+      showModal(modalDialog("Thanks for clicking me!", easyClose = TRUE))
+    })
 
     # alerts ------------------------------------------------------------------
 
@@ -244,7 +262,7 @@ shinyApp(
         )
       )
     })
-    
+
     observeEvent(input$hide_alert, {
       print("deleted")
       closeAlert(id = "alert_anchor")
@@ -255,7 +273,7 @@ shinyApp(
       alertStatus <- if (input$alert_anchor) "opened" else "closed"
       toastColor <- if (input$alert_anchor) "bg-lime" else "bg-fuchsia"
       toast(
-        title = sprintf("Alert succesfully %s!", alertStatus), 
+        title = sprintf("Alert succesfully %s!", alertStatus),
         options = list(
           class = toastColor,
           autohide = TRUE,
@@ -266,52 +284,51 @@ shinyApp(
 
     # tooltips, popovers ------------------------------------------------------
 
-    #observe({
+    # observe({
     #  addTooltip(
-    #    id = "controlbarToggle", 
+    #    id = "controlbarToggle",
     #    options = list(
-    #      title = "This toggles the right sidebar", 
+    #      title = "This toggles the right sidebar",
     #      placement = "bottom"
     #    )
     #  )
-    #})
+    # })
 
     # plots -------------------------------------------------------------------
-    
-    
-    
+
+
+
     output$bigPlot <- renderPlot({
       hist(rnorm(input$bigObs))
     })
-    
+
     output$plot <- renderPlot({
       hist(rnorm(input$obs))
     })
-    
+
     # this is not reactive but just for fixing the plot size on the client side.
     output$riverPlot <- renderEcharts4r({
-      river %>% 
-        e_charts(dates) %>% 
-        e_river(apples) %>% 
-        e_river(bananas) %>% 
-        e_river(pears) %>% 
-        e_tooltip(trigger = "axis") %>% 
+      river %>%
+        e_charts(dates) %>%
+        e_river(apples) %>%
+        e_river(bananas) %>%
+        e_river(pears) %>%
+        e_tooltip(trigger = "axis") %>%
         e_title("River charts", "(Streamgraphs)") %>%
         e_theme("shine")
     })
-    
+
     output$rosetype <- renderEcharts4r({
-      
-      plot <- mtcars %>% 
-        head() %>% 
-        dplyr::mutate(model = row.names(.)) %>% 
-        e_charts(model) %>% 
+      plot <- mtcars %>%
+        head() %>%
+        dplyr::mutate(model = row.names(.)) %>%
+        e_charts(model) %>%
         e_pie(hp, roseType = "radius")
       if (input$dark_mode) plot <- plot %>% e_theme(echarts_dark_theme$name)
       plot
     })
-    
-    
+
+
     observeEvent(input$current_tab, {
       if (input$current_tab == "cards") {
         showModal(modalDialog(
@@ -325,45 +342,45 @@ shinyApp(
         ))
       }
     })
-    
+
 
     # current theme info ---------------------------------------------------------
-    
+
     observeEvent(input$dark_mode, {
       toast(
         title = if (input$dark_mode) "Dark theme on!" else "Light theme on",
         options = list(position = "topRight", class = "bg-warning", autohide = TRUE)
       )
     })
-    
+
     # card API ----------------------------------------------------------------
-    
+
     output$cardAPIPlot <- renderPlot({
       if (input$mycard$maximized) {
-        hist(rnorm(input$obsAPI)) 
+        hist(rnorm(input$obsAPI))
       }
     })
-    
+
     observeEvent(input$triggerCard, {
       updateBox(id = "mycard", action = input$cardAction)
     })
-    
+
     observeEvent(input$update_box, {
       updateBox(
-        "mycard", 
-        action = "update", 
+        "mycard",
+        action = "update",
         options = list(
           title = tagList(h2("hello"), dashboardBadge(1, color = "primary")),
-          status = "warning", 
-          solidHeader = TRUE, 
-          width = 12, 
-          background = NULL, 
-          height = "900px", 
+          status = "warning",
+          solidHeader = TRUE,
+          width = 12,
+          background = NULL,
+          height = "900px",
           closable = FALSE
         )
       )
     })
-    
+
     observe({
       print(
         list(
@@ -373,32 +390,32 @@ shinyApp(
         )
       )
     })
-    
-    
+
+
     # card sidebar API --------------------------------------------------------
-    
+
     observeEvent(input$toggle_card_sidebar, {
       updateBoxSidebar("mycardsidebar")
     })
-    
+
     observeEvent(input$sidebar, {
       toastOpts$class <- if (input$sidebar) "bg-success" else "bg-danger"
       toast(
-        title = if (input$sidebar) "Sidebar opened!" else "Sidebar is closed!", 
+        title = if (input$sidebar) "Sidebar opened!" else "Sidebar is closed!",
         options = toastOpts
       )
     })
-    
-    
+
+
 
     # tabBox API  -------------------------------------------------------------
-    
+
     observeEvent(input$update_tabBox2, {
       updateBox("tabcard2_box", action = "toggleMaximize")
     })
 
     # controlbar input --------------------------------------------------------
-    
+
     observeEvent(input$controlbar, {
       toastOpts <- list(
         autohide = TRUE,
@@ -408,23 +425,23 @@ shinyApp(
       )
       toastOpts$class <- if (input$controlbar) "bg-success" else "bg-danger"
       toast(
-        title = if (input$controlbar) "Controlbar opened!" else "Controlbar closed!", 
+        title = if (input$controlbar) "Controlbar opened!" else "Controlbar closed!",
         options = toastOpts
       )
     })
-    
+
     observeEvent(input$controlbarToggle, {
       updateControlbar(id = "controlbar")
     })
-    
+
     observe({
       print(input$controlbar)
     })
-    
-    
+
+
     observeEvent(input$dropdown_item2, {
       toast(
-        title = "I am a toast!", 
+        title = "I am a toast!",
         options = list(
           autohide = TRUE,
           icon = "fas fa-home",
@@ -434,23 +451,23 @@ shinyApp(
         )
       )
     })
-    
+
 
     # update sidebar ----------------------------------------------------------
-    
+
     observeEvent(input$sidebarToggle, {
       updateSidebar(id = "sidebar")
     })
-    
+
     # user messages -----------------------------------------------------------
-    
+
     observeEvent(input$remove_message, {
       updateUserMessages("message", action = "remove", index = input$index_message)
     })
     observeEvent(input$add_message, {
       updateUserMessages(
-        "message", 
-        action = "add", 
+        "message",
+        action = "add",
         content = list(
           author = "David",
           date = "Now",
@@ -460,39 +477,39 @@ shinyApp(
         )
       )
     })
-    
+
     observeEvent(input$update_message, {
       updateUserMessages(
-        "message", 
-        action = "update", 
+        "message",
+        action = "update",
         index = input$index_message,
         content = list(
           text = tagList(
             appButton(
               inputId = "reload",
-              label = "Click me!", 
-              icon = icon("sync"), 
+              label = "Click me!",
+              icon = icon("sync"),
               dashboardBadge(1, color = "primary")
             )
           )
         )
       )
     })
-    
+
     observeEvent(input$reload, {
       showNotification("Yeah!", duration = 1, type = "default")
     })
-    
-    
-    
+
+
+
     # user menu ---------------------------------------------------------------
-    
+
     output$user <- renderUser({
       dashboardUser(
-        name = "Divad Nojnarg", 
-        image = "https://adminlte.io/themes/AdminLTE/dist/img/user2-160x160.jpg", 
+        name = "Divad Nojnarg",
+        image = "https://adminlte.io/themes/AdminLTE/dist/img/user2-160x160.jpg",
         title = "shinydashboardPlus",
-        subtitle = "Author", 
+        subtitle = "Author",
         footer = p("The footer", class = "text-center"),
         fluidRow(
           dashboardUserItem(
@@ -506,6 +523,5 @@ shinyApp(
         )
       )
     })
-    
   }
 )
