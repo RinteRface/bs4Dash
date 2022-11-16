237ba12c5df12a68b59ea28878cf013991ac63ba
Stephen Holsenbeck <stephen@virgalabs.io>
2022-11-16 12:46:04 -0800

    Add UU Reimports

---
+++ R/aaa_reimports.R
@@ -0,0 +1,28 @@
+#' @title Re-imports
+#' @name Re-imports
+#' @description Useful functions from other packages
+#' @importFrom rlang `%||%` `%|%`
+#' @importFrom UU `%|0|%` `%|try|%` `%|zchar|%` `%|legit|%` `%nin%`
+NULL
+
+
+#' @export
+rlang::`%||%`
+
+#' @export
+rlang::`%|%`
+
+#' @export
+UU::`%|try|%`
+
+#' @export
+UU::`%|0|%`
+
+#' @export
+UU::`%|zchar|%`
+
+#' @export
+UU::`%|legit|%`
+
+#' @export
+UU::`%nin%`
